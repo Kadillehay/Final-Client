6331d52a629d43507e57811277a9e5f99a95a18f
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-09-09 13:52:03 -0500

    contact.js done

---
+++ dist/js/contact.js
@@ -0,0 +1,31 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const contactMessage = {
+        name: name,
+        email: email,
+        message: message
+    };
+
+    fetch("http://localhost:8080/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(contactMessage)
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+
+        })
+        .catch(error => {
+
+            console.error(error);
+        });
+});
