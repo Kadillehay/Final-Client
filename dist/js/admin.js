52e0f174eb93fa35e93eca67f43082480b9326f7
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-09-12 12:13:16 -0500

    karen fucked up

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -1,29 +1,61 @@
-// admin.js
+console.log("top");
+let showAll = false;
+let globalData;
+let contactMessagesTable;
+function displayMessages(messages) {
+    globalData = messages;
+  
+}
 
-// Function to fetch contact messages
 function fetchContactMessages() {
-    fetch("http://localhost:8080/admin") // Replace with the actual endpoint
+    const showAllButton = document.getElementById("show-all-button");
+    fetch("http://localhost:8080/admin") 
       .then(response => response.json())
       .then(data => {
-        // Populate the Contact Messages section with data
-        const contactMessagesTable = document.querySelector("#contact-messages-table tbody");
-        contactMessagesTable.innerHTML = ''; // Clear existing data
-        
-        data.forEach(message => {
+        globalData = data;
+        contactMessagesTable = document.querySelector("#contact-messages-table tbody");
+        contactMessagesTable.innerHTML = ''; 
+        console.log(globalData);
+        globalData.forEach(message => {
           const row = document.createElement("tr");
           row.innerHTML = `
             <td class="px-4 py-2">${message.name}</td>
             <td class="px-4 py-2">${message.email}</td>
             <td class="px-4 py-2">${message.message}</td>
           `;
-          contactMessagesTable.appendChild(row);
+          contactMessagesTable.insertBefore(row, contactMessagesTable.firstChild);
         });
+        showAllFunction(globalData);
       })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-  
-  // Call the function to fetch contact messages when the page loads
+     
+}
+function showAllFunction(globalData){
+    document.querySelector("#contact-messages-table tbody").innerHTML="";
+        globalData.forEach(message => {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+            <td class="px-4 py-2">${message.name}</td>
+            <td class="px-4 py-2">${message.email}</td>
+            <td class="px-4 py-2">${message.message}</td>
+          `;
+          document.querySelector("#contact-messages-table tbody").insertBefore(row, document.querySelector("#contact-messages-table tbody").firstChild);
+        });
+        console.log(globalData);
+        console.log(showAll);
+        if(globalData) {
+        if (showAll) {
+            displayMessages(globalData.slice(0, 3));
+            showAllButton.textContent = "Show All";
+            showAll = false;
+        } else {
+            displayMessages(globalData);
+            showAllButton.textContent = "Show Less";
+            showAll = true;
+        }
+    }else{
+        console.error(" :( Data is undefined. Make sure your fetch request is successful.");
+    }
+    };
+    document.getElementById("show-all-button").addEventListener("click", showAllFunction(globalData));
   window.addEventListener("load", fetchContactMessages);
   
