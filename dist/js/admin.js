2f992d0123211abb88730580d624ab4e5457454c
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-12-07 10:13:19 -0600

    ...

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -1,52 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const token = JSON.parse(localStorage.getItem("token"));
-  fetch("http://localhost:8080/admin-dashboard", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((user) => {
-      if (!user.isAdmin) window.location.href = "./user-dashboard.html";
-      else fetchContactMessages();
-    })
+document.addEventListener("DOMContentLoaded", () => { 
+  const token = JSON.parse(localStorage.getItem("token")); 
+  fetch("http://localhost:8080/admin-dashboard", { 
+    method: "GET", 
+    headers: { 
+      "Content-Type": "application/json", 
+      Authorization: `Bearer ${token}`, 
+    }, 
+  }) 
+    .then((res) => res.json()) 
+    .then((user) => { 
+      if (!user.isAdmin) window.location.href = "./user-dashboard.html"; 
+      else fetchContactMessages(); 
+    }) 
     .catch((e) => {
-      console.error("Not authorized");
-      window.location.href = "./user-dashboard.html";
-    });
-});
+      console.error("Not authorized"); 
+      window.location.href = "./user-dashboard.html"; 
+    }); 
+}); 
 
-// const userID = JSON.parse(localStorage.getItem("user"))[0];
+// const userID = JSON.parse(localStorage.getItem("user"))[0]; 
 
-const showAllFarmButton = document.getElementById("show-all-farm-button");
-showAllFarmButton.addEventListener("click", () => {
-  const token = JSON.parse(localStorage.getItem("token"));
-
-  fetch("http://localhost:8080/get-all-farms", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((response) => response.json())
-    .then((farms) => {
-      console.log(farms);
-      const farmTable = document.querySelector("#farm-table");
-      farms.forEach((farm) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
+const showAllFarmButton = document.getElementById("show-all-farm-button"); 
+showAllFarmButton.addEventListener("click", () => { 
+  const token = JSON.parse(localStorage.getItem("token")); 
+ 
+  fetch("http://localhost:8080/get-all-farms", { 
+    method: "GET", 
+    headers: { 
+      "Content-Type": "application/json", 
+      Authorization: `Bearer ${token}`, 
+    }, 
+  }) 
+    .then((response) => response.json()) 
+    .then((farms) => { 
+      console.log(farms); 
+      const farmTable = document.querySelector("#farm-table"); 
+      farms.forEach((farm) => { 
+        const row = document.createElement("tr"); 
+        row.innerHTML = ` 
         <td class="px-4 py-2">${farm.farmName}</td>
         <td class="px-4 py-2">${farm.emailAddress}</td>
         <td class="px-4 py-2">${farm.firstName}</td>
         <td class="px-4 py-2">${farm.lastName}</td>
         `;
         farmTable.appendChild(row);
-      });
-    });
-});
+      }); 
+    }); 
+}); 
 
 // MESSAGE STUFF
 
@@ -60,14 +60,14 @@ function displayMessages(messages) {
 }
 function fetchContactMessages() {
   const showAllButton = document.getElementById("show-all-button");
-  const token = JSON.parse(localStorage.getItem("token"));
-  fetch("http://localhost:8080/admin-contact", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  const token = JSON.parse(localStorage.getItem("token")); 
+  fetch("http://localhost:8080/admin-contact", { 
+    method: "GET", 
+    headers: { 
+      "Content-Type": "application/json", 
+      Authorization: `Bearer ${token}`, 
+    }, 
+  }) 
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -130,4 +130,4 @@ function showAllFunction(globalData) {
 document
   .getElementById("show-all-button")
   .addEventListener("click", () => showAllFunction(globalData));
-// window.addEventListener("DOMContentLoaded", fetchContactMessages);
+// window.addEventListener("DOMContentLoaded", fetchContactMessages); 
