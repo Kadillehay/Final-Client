4bcdd6c6fa04a037603e4be652990ebd44fef8ca
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2024-02-09 16:39:04 -0600

    changed user-dashboard line 232. const farmName is now const farmNameField, so no duplicates

--- dist/js/user-dashboard.js
+++ dist/js/user-dashboard.js
@@ -223,14 +223,16 @@ viewFoodsBtn.addEventListener("click", () => {
 });
 
 // Selecting form fields
+
+//Karen Changed 232 to const farmNameField, because farmName is a const already.
 const firstName = document.getElementById("firstName")
 const lastName = document.getElementById("lastName")
 const emailAddress = document.getElementById("emailAddress")
 const password = document.getElementById("password")
-const farmName = document.getElementById("farmName")
+const farmNameField = document.getElementById("farmName")
 const submitBtn = document.querySelector('button[type="submit"]')
 
- const  validateFormFields = (firstName,lastName,emailAddress,password,farmName) => {
+ const  validateFormFields = (firstName,lastName,emailAddress,password,farmNameField) => {
   const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (emailAddress.value !== '' && !pattern.test(emailAddress.value)) {
     if(emailAddress) {
@@ -241,12 +243,12 @@ const submitBtn = document.querySelector('button[type="submit"]')
       return
     }
   }
-  if (firstName.value === '' || lastName.value === '' || emailAddress.value === '' || password.value === '' || farmName.value === '') {
+  if (firstName.value === '' || lastName.value === '' || emailAddress.value === '' || password.value === '' || farmNameField.value === '') {
       firstName.placeholder = "This field cannot be blank";
       lastName.placeholder = "This field cannot be blank";
       emailAddress.placeholder = "This field cannot be blank";
       password.placeholder = "This field cannot be blank";
-      farmName.placeholder = "This field cannot be blank";
+      farmNameField.placeholder = "This field cannot be blank";
       return false
     }  
     return true
@@ -261,7 +263,7 @@ const submitBtn = document.querySelector('button[type="submit"]')
     e.preventDefault();
    
     
-      const isValid = validateFormFields(firstName,lastName,emailAddress,password,farmName)
+      const isValid = validateFormFields(firstName,lastName,emailAddress,password,farmNameField)
       console.log(isValid)
       if (!isValid) return
 
@@ -271,7 +273,7 @@ const submitBtn = document.querySelector('button[type="submit"]')
         lastName:lastName.value,
         emailAddress:emailAddress.value,
         password:password.value,
-        farmName:farmName.value,
+        farmNameField:farmName.value,
       };
         fetch("https://final-api-v2-production.up.railway.app/register", {
           method: "POST",
@@ -288,13 +290,13 @@ const submitBtn = document.querySelector('button[type="submit"]')
               lastName.value = "";
               emailAddress.value = "";
               password.value = "";
-              farmName.value = "";
+              farmNameField.value = "";
               localStorage.setItem("token", JSON.stringify(data));
               alert("Registration successful!");
               const myDetails = data?.id;
               localStorage.setItem(
                 "user",
-                JSON.stringify([myDetails?.id, myDetails?.farmName])
+                JSON.stringify([myDetails?.id, myDetails?.farmNameField])
               );
 
               localStorage.setItem(
